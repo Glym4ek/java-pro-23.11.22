6eb1feed8f620eafc3944ca2c9f1d970f007179b
Valentyn Sharshon <woosay333@gmail.com>
2022-12-09 16:45:16 +0200

    fixes for Employee.java

--- src/main/java/com/gmail/woosay333/oop/introduction/employee/Employee.java
+++ src/main/java/com/gmail/woosay333/oop/introduction/employee/Employee.java
@@ -2,26 +2,46 @@ package com.gmail.woosay333.oop.introduction.employee;
 
 public class Employee {
 
-    private String fullName;
+    private String name;
+    private String surname;
+    private String patronymic;
     private String position;
     private int age;
     private String email;
     private String phoneNumber;
 
-    public Employee(String fullName, String position, int age, String email, String phoneNumber) {
-        this.fullName = fullName;
+    public Employee(String name, String surname, String patronymic, String position, int age, String email, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
         this.position = position;
         this.age = age;
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getName() {
+        return name;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
     }
 
     public String getPosition() {
@@ -59,7 +79,9 @@ public class Employee {
     @Override
     public String toString() {
         return "Employee{" +
-                "fullName='" + fullName + '\'' +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
                 ", position='" + position + '\'' +
                 ", age=" + age +
                 ", email='" + email + '\'' +
